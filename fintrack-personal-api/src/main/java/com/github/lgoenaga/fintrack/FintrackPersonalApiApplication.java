5e58c1708b9859f88adc4dac609720d340c16a38
Luis Goenaga <l_goenaga@hotmail.com>
2026-07-20 11:55:55 -0500

    chore: initial project structure

---
+++ fintrack-personal-api/src/main/java/com/github/lgoenaga/fintrack/FintrackPersonalApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lgoenaga.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackPersonalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackPersonalApiApplication.class, args);
+    }
+
+}
---
+++ fintrack-personal-api/src/test/java/com/github/lgoenaga/fintrack/FintrackPersonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lgoenaga.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackPersonalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
